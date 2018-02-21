f31f8c8735f697c5e8106125866f9a24af1f7338
djr4488 <djr4488@gmail.com>
2018-02-20 21:04:52 -0600

    updated to use retrofitee(separate project of mine) maybe i will have some time to work on this again(finally)

--- src/main/java/com/djr4488/wiichannelfeeder/forecastchannel/ForecastFeederController.java
+++ src/main/java/com/djr4488/wiichannelfeeder/forecastchannel/ForecastFeederController.java
@@ -7,8 +7,14 @@ import com.djr4488.wiichannelfeeder.forecastchannel.service.forecaststore.City;
 import com.djr4488.wiichannelfeeder.forecastchannel.service.forecaststore.Forecast;
 import com.djr4488.wiichannelfeeder.forecastchannel.service.forecaststore.ForecastStoreService;
 import com.djr4488.wiichannelfeeder.forecastchannel.service.forecaststore.RegionalForecast;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.djr.retrofit2ee.json.*;
 import org.slf4j.Logger;
+import retrofit2.Retrofit;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -29,10 +35,19 @@ import java.util.List;
 public class ForecastFeederController {
 	@Inject
 	private Logger log;
+	@JacksonModule(jacksonModules = {com.fasterxml.jackson.datatype.jsr310.JavaTimeModule.class})
+	@JacksonMapperFeature(features = {
+			@MapperFeatureConfig(feature = MapperFeature.REQUIRE_SETTERS_FOR_GETTERS, value = false),
+			@MapperFeatureConfig(feature = MapperFeature.AUTO_DETECT_GETTERS, value = true)})
+	@JacksonSerializationFeature(features = {
+			@SerializationFeatureConfig(feature = SerializationFeature.INDENT_OUTPUT, value = true)})
+	@JacksonDeserializationFeature(features = {
+			@DeserializationFeatureConfig(feature = DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, value = false)})
 	@Inject
+	@RetrofitJson(captureTrafficLogsPropertyName = "Darksky.enableTrafficLogging",
+			baseUrlPropertyName = "Darksky.baseUrl")
+	private Retrofit retrofitJson;
 	private DarkskyTransport darkskyTransport;
-	@Inject
-	private DarkskyCallback darkskyCallback;
 	@Inject
 	private ForecastStoreService forecastStoreService;
 	@Inject
@@ -41,30 +56,41 @@ public class ForecastFeederController {
 	//todo setup inject using a config service
 	private String key = "key";
 
+	@PostConstruct
+	private void postConstruct() {
+		darkskyTransport = retrofitJson.create(DarkskyTransport.class);
+	}
+
 	@Schedule(second="0", minute="0/1", hour="*")
-	public void getUpdatedForecastsByRegions() {
+	public void getUpdatedForecastsByRegions()
+	throws IOException {
 		String threadName = Thread.currentThread().getName();
+		DarkskyResponse resp =
+				darkskyTransport.getForecast(key, "38.8733", "-94.7752", "en", "us", "cst")
+					.execute()
+					.body();
+		log.info("getUpdateForecastByRegions() resp:{}", resp);
 		log.info("getUpdateForecastByRegions() started threadName:{}", threadName);
-		List<RegionalForecast> regionalForecasts = forecastStoreService.getRegions();
-		for (RegionalForecast regionalForecast : regionalForecasts) {
-			//TODO consider; denormalize db to avoid this loop?  only supporting 7 and not many regionalForecasts
-			//TODO so maybe not needed
-			forecastStoreService.deleteExistingForecastsForRegion(regionalForecast);
-			List<City> cities = regionalForecast.getCities();
-			for (City city : cities) {
-				DarkskyResponse response;
-				try {
-					response = darkskyTransport.getForecast(key, city.getLatitude(), city.getLongitude(),
-							regionalForecast.getLanguage(), regionalForecast.getUnits(), city.getTimezone()).execute().body();
-				} catch (IOException ioEx) {
-					log.error("getUpdatedForecastByRegions() failed obtaining forecast for city:{}", city, ioEx);
-					continue;
-				}
-				response.setRegionCode("049");
-				darkskyResponseBus.fire(response);
-				log.debug("getUpdatedForecastByRegions() response:{}", response);
-			}
-		}
+//		List<RegionalForecast> regionalForecasts = forecastStoreService.getRegions();
+//		for (RegionalForecast regionalForecast : regionalForecasts) {
+//			//TODO consider; denormalize db to avoid this loop?  only supporting 7 and not many regionalForecasts
+//			//TODO so maybe not needed
+//			forecastStoreService.deleteExistingForecastsForRegion(regionalForecast);
+//			List<City> cities = regionalForecast.getCities();
+//			for (City city : cities) {
+//				DarkskyResponse response;
+//				try {
+//					response = darkskyTransport.getForecast(key, city.getLatitude(), city.getLongitude(),
+//							regionalForecast.getLanguage(), regionalForecast.getUnits(), city.getTimezone()).execute().body();
+//				} catch (IOException ioEx) {
+//					log.error("getUpdatedForecastByRegions() failed obtaining forecast for city:{}", city, ioEx);
+//					continue;
+//				}
+//				response.setRegionCode("049");
+//				darkskyResponseBus.fire(response);
+//				log.debug("getUpdatedForecastByRegions() response:{}", response);
+//			}
+//		}
 	}
 
 	public File getForecastFile(String region, String file) {
